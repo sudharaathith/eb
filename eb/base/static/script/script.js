3c5140fbdc2fc8b2b667985384d4e7e2f1758942
sudhar <t.sudharaathith@gmail.com>
2022-08-26 23:32:35 +0530

    Dynamic Table

--- eb/base/static/script/script.js
+++ eb/base/static/script/script.js
@@ -20,8 +20,55 @@ const config = {
     displayModeBar: false, // this is the line that hides the bar.
   };
   
+function cost(unit){
+  if(unit<=100)
+      return 0;
+  else if(unit<=200){
+    
+      unit -= 100;
+      return (unit*1.5)+20
+  }
+  else if(unit <=500){
+      unit -= 200;
+      return (unit*3)+200+30
+  }
+  else{
+      unit -= 500;
+      return (unit*6.6)+1730+50
+  }
+}
 
 // Display using Plotly
 Plotly.newPlot("myPlot", data, layout, config);
+domine = document.getElementById('domine').innerText;
+var vul, i = 0,table = document.getElementById('data');
+$.getJSON('/api/getmt-613:0',(json)=>{
+  for(let i = 0; i<Object.create(json).length;i++){
+    let date = new Date(json[i]['date']);
+  let unit = json[i]['unit'];
+  let row = table.insertRow(i+1);
+  row.insertCell(0).innerText = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+  row.insertCell(1).innerText = unit;
+  row.insertCell(2).innerText = cost(unit);
+  if(i == 0){
+    row.insertCell(3).innerText=0
+    row.insertCell(4).innerText=0
+    row.insertCell(5).innerText=0 
+  }
+  else{
+    let days = (date-new Date(json[i-1]['date']))/(1000 * 60 * 60 * 24);
+    row.insertCell(3).innerText=days
+    row.insertCell(4).innerText=Math.round((unit-json[i-1]['unit'])/days*1000)/1000
+    row.insertCell(5).innerText= Math.round(cost(unit)/days*1000)/1000
+  }
+  }
+  })
 
-console.log(document.getElementsByClassName('domine'))
+
+let  count =0;
+for(i in vul){
+  
+
+}
+
+console.log(domine)
--- eb/base/views.py
+++ eb/base/views.py
@@ -2,11 +2,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.template import loader
+from requests import Request, request
 
-def index(request: HttpRequest):
+def index(request: Request):
   template = loader.get_template('index.html')
   context = {
-    'domine' : 'hi'
+    'domine' : request.headers['Host']
   }
   return HttpResponse(template.render(context, request))
 # Create your views here.
